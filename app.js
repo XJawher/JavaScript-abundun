5feb006f017f8e37866594d1aff8ca4f0a175230
xiancai <lplee7830@gmail.com>
2017-08-29 22:28:09 +0800

    构造函数和扩展

--- app.js
+++ app.js
@@ -33,46 +33,71 @@
 if语句     
 逻辑运算  */
 /*变量计算*/
-var aa = 100 +10
-console.log('aa='+aa)
+// var aa = 100 +10
+// console.log('aa='+aa)
 
-/*字符串拼接*/
-var bb = 100 + '10'
-console.log('bb='+bb)
+// /*字符串拼接*/
+// var bb = 100 + '10'
+// console.log('bb='+bb)
 
-/*== 运算符 */
-console.log(100 == '100')
-console.log(0 == '')
-console.log(null == undefined)
+// /*== 运算符 */
+// console.log(100 == '100')
+// console.log(0 == '')
+// console.log(null == undefined)
 
-/*if语句 */
-var a = true
-if (a) {
-  console.log('true')
-}else{
-  console.log('false')
-}//true
-var b = 100
-if (b) {
-  console.log('true')
-}else{
-  console.log('false')
-}//true
-var c = ''
-if (c) {
-  console.log('true')
-}else{
-  console.log('false')
-}//false
+// /*if语句 */
+// var a = true
+// if (a) {
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }//true
+// var b = 100
+// if (b) {
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }//true
+// var c = ''
+// if (c) {
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }//false
 
-/*逻辑运算*/
-console.log(10 && 0)//0
-console.log('' || '123')//"123"
-console.log(!window.ss)//true
+// /*逻辑运算*/
+// console.log(10 && 0)//0
+// console.log('' || '123')//"123"
+// console.log(!window.ss)//true
 
-var a = 100
+// var a = 100
 
-console.log(!!a)
+// console.log(!!a)
+
+/*原型和原型链*/
+/*1问：怎么准确判断一个变量是数组类型*/
+
+/*2问：写一个原型链的继承的例子*/
+/*3问：描 new 一个对象的过程*/
+/*4问：zepto 或者其他框架源码中如何使用原型链*/
+
+/*构造函数*/
+function Foo(age,name) {
+  this.age = age
+  this.name = name
+  this.class = 'class-1'
+  //return this //默认有这一行
+}
+
+var f = new Foo('zhangsan',20)
+var f1 = new Foo('lisi',22)
+
+console.log(f)
+console.log(f1)
+
+
+
+
 
 
 
